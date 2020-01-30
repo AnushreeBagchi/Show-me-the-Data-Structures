72fa17c9e58f9666ff8ed4c47fdffc0cf613b230
Anushree Bagchi <anushreebagchi4791@gmail.com>
2020-01-30 23:44:23 +0530

    Blockchain added

---
+++ Blockchain.py
@@ -0,0 +1,51 @@
+import hashlib
+import time
+
+class Block:
+    def __init__(self, timestamp, data, previous_hash):
+      self.timestamp = timestamp
+      self.data = data
+      self.previous_hash = previous_hash
+      self.hash = self.calc_hash()
+      self.next = None
+    def calc_hash(self):
+      sha = hashlib.sha256()
+      hash_str = "We are going to encode this string of data!".encode('utf-8')
+      sha.update(hash_str)
+      return sha.hexdigest()
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def append(self, data):
+        if self.head is None:
+            previous_hash = None
+            self.head = Block(time.time(), data, previous_hash)
+            self.tail = self.head
+            return
+        previous_hash = self.tail.hash
+        self.tail.next = Block(time.time(), data, previous_hash)
+        self.tail = self.tail.next
+
+    
+
+data1 = "Data for first block"
+data2 = "Data for second block"
+data3 = "Data for third block"
+
+linkedlist = LinkedList()
+linkedlist.append(data1)
+linkedlist.append(data2)
+linkedlist.append(data3)
+
+def test(ll):
+    node = ll.head
+    while node.next:
+        if node.hash != node.next.previous_hash:
+            return "Test Failed"
+        node = node.next
+    return "Test Passed"
+
+print(test(linkedlist))
