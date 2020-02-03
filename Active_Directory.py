73ff143bf9ea70aeb8792c5a1f4e531b99df0166
Anushree Bagchi <anushreebagchi4791@gmail.com>
2020-02-03 14:54:01 +0530

    Active directory updated

--- Active_Directory.py
+++ Active_Directory.py
@@ -29,22 +29,37 @@ parent.add_user("Parent user")
 child.add_user("Child user")
 
 def is_user_in_group(input_user, group):
+    if group is None:
+        raise ValueError('Please enter a valid group')
+    userList = return_users_in_group(group, list())
+    if input_user in userList:
+        return True
+    return False
+
+def return_users_in_group(group, user_list):
     group_users = group.get_users()
     child_groups = group.get_groups()
     if group_users != []:
         for group_user in group_users:
-            if group_user == input_user:
-                return True
-    elif child_groups != []:
+            user_list.append(group_user)
+    if child_groups != []:
         child_groups = group.get_groups()
         for child_group in child_groups:
-            is_user_in_group(input_user, child_group)
-    return False
+            return_users_in_group(child_group, user_list)
+    return user_list
 
     
 # Test cases
-print("Pass" if is_user_in_group(sub_child_user, parent) == False else "Fail")
+print("Pass" if is_user_in_group(sub_child_user, parent) == True else "Fail")
 print("Pass" if is_user_in_group("Parent user", parent) == True else "Fail")
 print("Pass" if is_user_in_group("Parent user", child) == False else "Fail")
 
-print("Pass" if is_user_in_group("", child) == False else "Fail") #edge case
+print("Pass" if is_user_in_group("", child) == False else "Fail") #edge case when user is not provided
+
+def test5():
+    try:
+        is_user_in_group("Parent user", None)
+    except ValueError as err:
+        print("Pass")
+
+test5()
